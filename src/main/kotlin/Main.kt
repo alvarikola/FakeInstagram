189f2590e8b9b27c9f90902838c2141732994dbe
alvarikola <alvaroe484@gmail.com>
2024-11-07 13:59:18 +0000

    empezada la interfaz

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,4 +1,6 @@
 import androidx.compose.desktop.ui.tooling.preview.Preview
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
 import androidx.compose.material.Button
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
@@ -7,22 +9,52 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.window.Window
 import androidx.compose.ui.window.application
 
 @Composable
 @Preview
 fun App() {
-    var text by remember { mutableStateOf("Hello, World!") }
 
-    MaterialTheme {
-        Button(onClick = {
-            text = "Hello, Desktop!"
-        }) {
-            Text(text)
+    Column {
+        Row {
+            Text(
+                text = "Instagram"
+            )
+        }
+        Row {
+            Text(
+                text = "Historias"
+            )
+        }
+        Row {
+            historias.forEach{ historia ->
+                Text(
+                    text = historia.fotoCuenta,
+                    modifier = Modifier.padding(10.dp)
+                )
+            }
+        }
+        Row(modifier = Modifier.fillMaxSize().background(Color.Yellow).padding(20.dp)) {
+            Column(modifier = Modifier.background(Color.Red).fillMaxHeight()) {
+                Text(text = "Publicaciones")
+            }
+            Column (modifier = Modifier.background(Color.Blue)){
+                Text(text = "Sugerencias")
+                cuentaSugerida.forEach{ cuentaSugerida ->
+                    Row {
+                        Text(text = cuentaSugerida.fotoCuenta)
+                        Text(text = cuentaSugerida.nombreCuenta)
+                    }
+                }
+            }
         }
     }
 }
+
 
 fun main() = application {
     Window(onCloseRequest = ::exitApplication) {
