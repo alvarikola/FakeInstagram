ed6e86078eb98aa9e7d7a7fbcd0f660e9c0f1ba3
alvarikola <alvaroe484@gmail.com>
2024-11-08 13:56:30 +0000

    mejorados colores

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -4,6 +4,7 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Button
 import androidx.compose.material.Divider
 import androidx.compose.material.MaterialTheme
@@ -13,11 +14,15 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.BiasAlignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.draw.shadow
 import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.graphics.*
+import androidx.compose.ui.layout.HorizontalAlignmentLine
+import androidx.compose.ui.layout.VerticalAlignmentLine
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.font.FontWeight.Companion.Bold
@@ -30,13 +35,13 @@ import androidx.compose.ui.window.application
 @Preview
 fun App() {
 
-    Column (modifier = Modifier.background(Color(0xFFadd8e6)).padding(20.dp)){
+    Column (modifier = Modifier.background(Color(0xFFC8BDE0)).padding(20.dp)){
         Row {
             Text(
                 text = "Instagram",
                 fontWeight = FontWeight.Bold,
                 fontSize = 35.sp,
-                color = Color(0xFF41b441),
+                color = Color(0xFF7275E0),
                 modifier = Modifier.padding(bottom = 30.dp)
             )
         }
@@ -44,7 +49,7 @@ fun App() {
             Text(
                 text = "Historias",
                 fontSize = 25.sp,
-                color = Color(0xFF41b441),
+                color = Color(0xFF7275E0),
                 modifier = Modifier.padding(bottom = 20.dp)
 
             )
@@ -54,8 +59,8 @@ fun App() {
                 width = 1.dp,
                 shape = RectangleShape,
                 brush = Brush.linearGradient(
-                    colors = listOf(Color.Black, Color.Black)
-                )
+                    colors = listOf(Color(0xFF9372E0), Color(0xFF9372E0))
+                ),
             )
             .background(Color.White)
             .fillMaxWidth()
@@ -71,7 +76,7 @@ fun App() {
                             width = 3.dp,
                             shape = CircleShape,
                             brush = Brush.linearGradient(
-                                colors = listOf(Color.Blue, Color.Green)
+                                colors = listOf(Color(0xFFDB72E0), Color(0xFF7275E0))
                             )
                         )
                         .width(100.dp)
@@ -81,11 +86,18 @@ fun App() {
         }
         Divider(modifier = Modifier.padding(top=10.dp))
 
-        Row(modifier = Modifier.fillMaxSize().background(Color.Yellow).padding(20.dp)) {
-            Column(modifier = Modifier.background(Color.Red).fillMaxHeight().weight(1F)) {
+        Row(modifier = Modifier.fillMaxSize().background(Color.White).padding(20.dp)) {
+            Column(modifier = Modifier.background(Color.Red).fillMaxHeight().weight(0.6f)) {
                 Text(text = "Publicaciones")
             }
-            Column (modifier = Modifier.background(Color.Blue).weight(1F)){
+            Divider(
+                color = Color.Red,
+                modifier = Modifier
+                    .fillMaxHeight()  //fill the max height
+                    .width(10.dp)
+                    .padding(start=10.dp)
+            )
+            Column (modifier = Modifier.background(Color.Blue).weight(0.4f)){
                 Text(text = "Sugerencias")
                 cuentaSugerida.forEach{ cuentaSugerida ->
                     Row {
